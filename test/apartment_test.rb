0ace40035cfa52d4722bbab502630eccc127e1f3
Ross Perry <perryr16@gmail.com>
2020-03-31 09:14:41 -0600

    tested apartment details and add renter

---
+++ test/apartment_test.rb
@@ -0,0 +1,30 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/renter'
+require './lib/apartment'
+class ApartmentTest < Minitest::Test
+
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_it_returns_details
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+    assert_nil unit1.renter
+  end
+
+  def test_it_can_add_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    unit1.add_renter(renter1)
+    assert_equal renter1, unit1.renter
+    binding.pry
+  end
+
+end
