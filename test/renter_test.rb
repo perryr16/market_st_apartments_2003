423ae2b1266bb40fcceb45c9e24a946a09e0fdf8
Ross Perry <perryr16@gmail.com>
2020-03-31 09:07:19 -0600

    test returns renter name

---
+++ test/renter_test.rb
@@ -0,0 +1,54 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/renter'
+require './lib/apartment'
+class RenterTest < Minitest::Test
+
+  def test_it_exists
+    renter1 = Renter.new("Jessie")
+
+    assert_instance_of Renter, renter1
+  end
+
+  def test_it_returns_renter_name
+    renter1 = Renter.new("Jessie")
+
+    assert_equal "Jessie", renter1.name
+  end
+
+end
+
+# require './lib/renter'
+# # => true
+#
+# require './lib/apartment'
+# # => true
+#
+# # => #<Renter:0x00007a15c93af80...>
+#
+# renter1.name
+# # => "Jessie"
+#
+# unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+# # => #<Apartment:0x00007fb3ecaae7c0...>
+#
+# unit1.number
+# # => "A1"
+#
+# unit1.monthly_rent
+# # => 1200
+#
+# unit1.bathrooms
+# # => 1
+#
+# unit1.bedrooms
+# # => 1
+#
+# unit1.renter
+# # => nil
+#
+# unit1.add_renter(renter1)
+#
+# unit1.renter
+# # => #<Renter:0x00007fb3ee106ce8...>
